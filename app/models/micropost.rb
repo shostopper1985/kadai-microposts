f35d0d715bf5399339df3344f914d4c190a435d5
sho.yasuda <shostopper34@yahoo.co.jp>
2017-05-04 22:01:47 +0000

    remove

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,9 +5,5 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
   
   has_many :favorites
-  has_many :favorites_user, through: :favorites, source: :micropost
-  
-  def feed_favorites
-    Micropost.where(id: favorites_user_ids)
-  end
+
 end
